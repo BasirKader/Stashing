9ff766a3d515795bdcc56a8d7f3920e31f252bb1
BasirKader <basirkader33@gmail.com>
2022-05-15 12:05:37 -0400

    The car is ready

--- src/StashPackage/Main.java
+++ src/StashPackage/Main.java
@@ -2,7 +2,13 @@ package StashPackage;
 
 public class Main {
     public static void main(String[] args) {
-        BasicFeatures car=new BasicFeatures();
-       
+        BasicFeatures Audi=new BasicFeatures();
+       Audi.color="red";
+       Audi.doors="4";
+       Audi.engineCapac="v8";
+       Audi.headLight="blue";
+       Audi.regNo="12345";
+
+        
     }
 }
