8e4a76ebe84710962c2c6c7dc53d7b4c4edff219
BasirKader <basirkader33@gmail.com>
2022-05-15 11:03:31 -0400

    the basic features are ready

---
+++ src/StashPackage/BasicFeatures.java
@@ -0,0 +1,10 @@
+package StashPackage;
+
+public class BasicFeatures {
+
+    String engineCapac;
+    String color;
+    String doors;
+    String headLight;
+    String regNo;
+}
--- src/StashPackage/Main.java
+++ src/StashPackage/Main.java
@@ -1,8 +1,8 @@
 package StashPackage;
 
-public class stash {
+public class Main {
     public static void main(String[] args) {
-        System.out.println("stash");
-
+        BasicFeatures car=new BasicFeatures();
+       
     }
 }
